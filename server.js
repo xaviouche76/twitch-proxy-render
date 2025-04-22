81b974329eb50e645e78ec4101f44e91ffdbcd10
xaviouche76 <ton@email.com>
2025-04-22 05:32:13 +0200

    correction erreur

--- server.js
+++ server.js
@@ -126,10 +126,6 @@ app.get('/init-db', async (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
 app.listen(PORT, () => {
   console.log(`Serveur proxy Twitch lance sur le port ${PORT}`);
 });

f0f3d34bde60c4f479d6227e312c26590491abcb
xaviouche76 <ton@email.com>
2025-04-22 04:12:46 +0200

    Connexion PostgreSQL fixée et ajout du /register-streamer

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 app.use(express.json());
